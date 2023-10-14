a9bbcd20761dd23afa7733ae1d005e985547d3df
Damilare Oyedeji <damoye81@gmail.com>
2023-10-14 03:11:39 +0100

    fix

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: 'https://fuglydami.github.io/adewale-wedding',
+};
+
+module.exports = nextConfig;
